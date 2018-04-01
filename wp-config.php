5dd18274fce9e9c384bc9aba26fca360695b6a71
Martin Tracey <matracey@outlook.ie>
2018-04-01 20:29:15 +0100

    Added wp-config that uses Azure environment variables.

--- wp-config.php
+++ wp-config.php
@@ -20,22 +20,22 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define('DB_NAME', 'database_name_here');
+define('DB_NAME', 		getenv("DATABASE_NAME"));
 
 /** MySQL database username */
-define('DB_USER', 'username_here');
+define('DB_USER', 		getenv("DATABASE_USERNAME"));
 
 /** MySQL database password */
-define('DB_PASSWORD', 'password_here');
+define('DB_PASSWORD', 	getenv("DATABASE_PASSWORD"));
 
 /** MySQL hostname */
-define('DB_HOST', 'localhost');
+define('DB_HOST', 		getenv("DATABASE_HOST"));
 
 /** Database Charset to use in creating database tables. */
-define('DB_CHARSET', 'utf8');
+define('DB_CHARSET', 	getenv("DATABASE_CHARSET"));
 
 /** The Database Collate type. Don't change this if in doubt. */
-define('DB_COLLATE', '');
+define('DB_COLLATE', 	getenv("DATABASE_COLLATE"));
 
 /**#@+
  * Authentication Unique Keys and Salts.
@@ -46,14 +46,14 @@ define('DB_COLLATE', '');
  *
  * @since 2.6.0
  */
-define('AUTH_KEY',         'put your unique phrase here');
-define('SECURE_AUTH_KEY',  'put your unique phrase here');
-define('LOGGED_IN_KEY',    'put your unique phrase here');
-define('NONCE_KEY',        'put your unique phrase here');
-define('AUTH_SALT',        'put your unique phrase here');
-define('SECURE_AUTH_SALT', 'put your unique phrase here');
-define('LOGGED_IN_SALT',   'put your unique phrase here');
-define('NONCE_SALT',       'put your unique phrase here');
+define('AUTH_KEY',         getenv("AUTH_KEY"));
+define('SECURE_AUTH_KEY',  getenv("SECURE_AUTH_KEY"));
+define('LOGGED_IN_KEY',    getenv("LOGGED_IN_KEY"));
+define('NONCE_KEY',        getenv("NONCE_KEY"));
+define('AUTH_SALT',        getenv("AUTH_SALT"));
+define('SECURE_AUTH_SALT', getenv("SECURE_AUTH_SALT"));
+define('LOGGED_IN_SALT',   getenv("LOGGED_IN_SALT"));
+define('NONCE_SALT',       getenv("NONCE_SALT"));
 
 /**#@-*/
 
